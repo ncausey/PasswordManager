9c8e26afd848e1a533141b25fce4dc65c6adb309
ncausey <ncausey@outlook.com>
2015-11-18 22:10:54 -0500

    Initial implementation of password list

--- app/src/main/java/osu/passwordmanager/PasswordList.java
+++ app/src/main/java/osu/passwordmanager/PasswordList.java
@@ -1,28 +1,63 @@
 package osu.passwordmanager;
 
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
 
-public class PasswordList extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PasswordList extends ListActivity {
+
+    List<Map<String, String>> passList = new ArrayList<Map<String, String>>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_password_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // We'll define a custom screen layout here (the one shown above), but
+        // typically, you could just use the standard ListActivity layout.
+        setContentView(R.layout.activity_password_list);
+
+        initList();
+
+        ListView lv = (ListView) findViewById(R.id.passwordList);
+
+        // Now create a new list adapter bound to the cursor.
+        // SimpleListAdapter is designed for binding to a Cursor.
+        ListAdapter adapter = new SimpleAdapter(this, passList, android.R.layout.simple_list_item_2, new String[] {"password"}, new int[] {android.R.id.text1});
+
+        // Bind to our new adapter.
+        lv.setAdapter(adapter);
+    }
+
+    private void initList() {
+        passList.add(createPasswordMap("Google", "test"));
+        passList.add(createPasswordMap("Facebook", "test2"));
+    }
+    private HashMap<String, String> createPasswordMap(String key, String value) {
+        HashMap<String, String> passMap = new HashMap<String, String>();
+        passMap.put(key, value);
+        return passMap;
     }
 }

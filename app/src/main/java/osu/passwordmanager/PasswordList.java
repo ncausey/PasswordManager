fca6fbcbed1a4b7c28f2a7d323d73398fb467e65
ncausey <ncausey@outlook.com>
2015-11-20 20:58:33 -0500

    Initial implementation of password list

--- app/src/main/java/osu/passwordmanager/PasswordList.java
+++ app/src/main/java/osu/passwordmanager/PasswordList.java
@@ -6,15 +6,34 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PasswordList extends AppCompatActivity {
+
+    List<Map<String, String>> passList = new ArrayList<Map<String, String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_password_list);
+        setContentView(R.layout.content_password_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initList();
+
+        ListView lv = (ListView) findViewById(R.id.passwordList);
+
+        ListAdapter adapter = new SimpleAdapter(this, passList, android.R.layout.simple_list_item_1, new String[] {"password"}, new int[] {android.R.id.text1});
+
+        // Bind to our new adapter.
+        lv.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +43,16 @@ public class PasswordList extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void initList() {
+        passList.add(createPasswordMap("password", "test"));
+        passList.add(createPasswordMap("password", "test2"));
+    }
+
+    private HashMap<String, String> createPasswordMap(String key, String value) {
+        HashMap<String, String> passMap = new HashMap<String, String>();
+        passMap.put(key, value);
+        return passMap;
     }
 }
